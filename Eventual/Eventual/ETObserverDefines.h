be2a1e29f2f397136d456cab37b1c46b74e7e1b1
Peng Wang <peng@pengxwang.com>
2013-11-24 18:50:55 -0800

    Add observer defines.

---
+++ Eventual/Eventual/ETObserverDefines.h
@@ -0,0 +1,11 @@
+//
+//  ETObserverDefines.h
+//  Eventual
+//
+//  Created by Nest Master on 11/24/13.
+//  Copyright (c) 2013 Hashtag Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+static void * ETContext;
---
+++ Eventual/Eventual/ETObserverDefines.m
@@ -0,0 +1,11 @@
+//
+//  ETObserverDefines.m
+//  Eventual
+//
+//  Created by Nest Master on 11/24/13.
+//  Copyright (c) 2013 Hashtag Studio. All rights reserved.
+//
+
+#import "ETObserverDefines.h"
+
+static void * ETContext = &ETContext;
