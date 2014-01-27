496cf670da615eb3382614ed7f274b9cd6eed413
Peng Wang <peng@pengxwang.com>
2014-01-27 03:01:20 -0800

    Add and set up initial icon font.

---
+++ Eventual/Eventual/ETIconDefines.h
@@ -0,0 +1,16 @@
+//
+//  ETIconDefines.h
+//  Eventual
+//
+//  Created by Nest Master on 1/27/14.
+//  Copyright (c) 2014 Hashtag Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const ETIconCheckCircle;
+extern NSString *const ETIconClock;
+extern NSString *const ETIconMapPin;
+extern NSString *const ETIconTrash;
+extern NSString *const ETIconLeftArrow;
+extern NSString *const ETIconCross;
---
+++ Eventual/Eventual/ETIconDefines.m
@@ -0,0 +1,16 @@
+//
+//  ETIconDefines.m
+//  Eventual
+//
+//  Created by Nest Master on 1/27/14.
+//  Copyright (c) 2014 Hashtag Studio. All rights reserved.
+//
+
+#import "ETIconDefines.h"
+
+NSString *const ETIconCheckCircle = @"\ue602";
+NSString *const ETIconClock = @"\ue600";
+NSString *const ETIconCross = @"\ue605";
+NSString *const ETIconLeftArrow = @"\ue604";
+NSString *const ETIconMapPin = @"\ue601";
+NSString *const ETIconTrash = @"\ue603";
