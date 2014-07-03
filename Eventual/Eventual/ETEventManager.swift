70389876c34bdf7daa8206813614794b9e5f56a2
Peng Wang <peng@pengxwang.com>
2014-07-02 22:34:44 -0700

    Port more of EventManager.
    Include minor syntax fixes and cleanup.

--- Eventual/Eventual/ETEventManager.swift
+++ Eventual/Eventual/ETEventManager.swift
@@ -27,19 +27,24 @@ let ETEntityCollectionDatesKey = "dates"
 let ETEntityCollectionDaysKey = "days"
 let ETEntityCollectionEventsKey = "events"
 
+typealias ETFetchEventsCompletionHandler = () -> Void
+
 @objc(ETEventManager) class EventManager: NSObject {
     
-    var _mutableEvents: EKEvent[] = []
-    var events: EKEvent[] {
-    return self._mutableEvents
-    }
+    var store: EKEventStore!
+    var operationQueue: NSOperationQueue!
+    
+    var calendars: EKCalendar[]?
+    var calendar:EKCalendar?
+    
+    var events: EKEvent[]?
     
     @lazy var eventsByMonthsAndDays: Dictionary<String, AnyObject[]> = {
         var months: Dictionary<String, AnyObject[]> = [:]
         var monthsDates: NSDate[] = []
         var monthsDays: Dictionary<String, AnyObject[]>[] = []
         let calendar = NSCalendar.currentCalendar()
-        for event in self.events {
+        for event in self.events! {
             let monthComponents = calendar.components(.CalendarUnitMonth | .YearCalendarUnit, fromDate: event.startDate)
             let dayComponents = calendar.components(.DayCalendarUnit | .MonthCalendarUnit | .YearCalendarUnit, fromDate: event.startDate)
             let monthDate = calendar.dateFromComponents(monthComponents)
@@ -74,11 +79,80 @@ let ETEntityCollectionEventsKey = "events"
         }
         months[ETEntityCollectionDatesKey] = monthsDates
         months[ETEntityCollectionDaysKey] = monthsDays
-        return months;
+        return months
     }()
     
     init() {
         super.init()
+        self.store = EKEventStore()
+        self.operationQueue = NSOperationQueue()
     }
-   
+    
+    func completeSetup() {
+        self.store.requestAccessToEntityType(EKEntityTypeEvent, completion: { (granted: Bool, accessError: NSError!) -> Void in
+            var userInfo: Dictionary<String, AnyObject> = [:]
+            userInfo[ETEntityAccessRequestNotificationTypeKey] = EKEntityTypeEvent
+            if granted {
+                userInfo[ETEntityAccessRequestNotificationResultKey] = ETEntityAccessRequestNotificationGranted
+                self.calendars = self.store.calendarsForEntityType(EKEntityTypeEvent) as? EKCalendar[]
+                self.calendar = self.store.defaultCalendarForNewEvents
+            } else if !granted {
+                userInfo[ETEntityAccessRequestNotificationResultKey] = ETEntityAccessRequestNotificationDenied
+            } else if accessError {
+                userInfo[ETEntityAccessRequestNotificationResultKey] = ETEntityAccessRequestNotificationError
+                userInfo[ETEntityAccessRequestNotificationErrorKey] = accessError
+            }
+            NSNotificationCenter.defaultCenter()
+                .postNotificationName(ETEntityAccessRequestNotification, object: self, userInfo: userInfo)
+        })
+    }
+    
+    func fetchEventsFromDate(startDate: NSDate = NSDate.date(),
+                             untilDate endDate: NSDate,
+                             completion: ETFetchEventsCompletionHandler) -> NSOperation {
+        let predicate = self.store.predicateForCompletedRemindersWithCompletionDateStarting(
+            startDate, ending: endDate, calendars: self.calendars)
+        let fetchOperation = NSBlockOperation({ () -> Void in
+            self.events = self.store.eventsMatchingPredicate(predicate) as? EKEvent[]
+        })
+        fetchOperation.queuePriority = NSOperationQueuePriority.VeryHigh
+        let completionOperation = NSBlockOperation(completion)
+        completionOperation.addDependency(fetchOperation)
+        self.operationQueue.addOperation(fetchOperation)
+        NSOperationQueue.mainQueue().addOperation(completionOperation)
+        return fetchOperation
+    }
+    
+    func saveEvent(event: EKEvent, error: NSErrorPointer) -> Bool {
+        if !self.validateEvent(event, error: error) { return false }
+        var didSave = self.store.saveEvent(event, span: EKSpanThisEvent, commit: true, error: error)
+        if didSave {
+            self._addEvent(event)
+            var userInfo: Dictionary<String, AnyObject> = [:]
+            userInfo[ETEntityOperationNotificationTypeKey] = EKEntityTypeEvent
+            userInfo[ETEntityOperationNotificationDataKey] = event
+            NSNotificationCenter.defaultCenter()
+                .postNotificationName(ETEntitySaveOperationNotification, object: self, userInfo: userInfo)
+        }
+        return didSave
+    }
+    
+    func validateEvent(event: EKEvent, error: NSErrorPointer) -> Bool {
+        // TODO
+        return true
+    }
+    
+    func _addEvent(event: EKEvent) -> Bool {
+        var didAdd = false
+        if var events = self.events {
+            let bridgedEvents = events.bridgeToObjectiveC()
+            if bridgedEvents.containsObject(event) {
+                events.append(event)
+                bridgedEvents.sortedArrayUsingSelector(Selector("compareStartDateWithEvent:"))
+                didAdd = true
+            }
+        }
+        return didAdd
+    }
+
 }
