6f7b603f5c10cfaa7b9039964d73c7217d749910
Peng Wang <peng@pengxwang.com>
2014-07-21 02:22:36 -0700

    Consolidate defines to one file, port accessibility defines.

---
+++ Eventual/Eventual/Defines.swift
@@ -0,0 +1,26 @@
+//
+//  ETDefines.swift
+//  Eventual
+//
+//  Created by Peng Wang on 7/9/14.
+//  Copyright (c) 2014 Hashtag Studio. All rights reserved.
+//
+
+import Foundation
+
+// MARK: Error
+
+let ETErrorDomain = "ETErrorDomain"
+
+let ETErrorCodeGeneric = 0
+let ETErrorCodeInvalidObject = 1
+
+// MARK: Accessibility
+
+let ETLabelFormatDayCell = "Day Cell At Section %d Item %d"
+let ETLabelDayEvents = "Day Events"
+let ETLabelFormatDayOption = "Day Option Named %@"
+let ETLabelEventScreenTitle = "Event Screen Title"
+let ETLabelMonthDays = "Eventful Days By Month"
+let ETLabelMonthScreenTitle = "Month Screen Title"
+let ETLabelNavigationBack = "Back"
