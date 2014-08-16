f4a915a6dcaf3d39d595e79c80393f600c2b66fd
Peng Wang <peng@pengxwang.com>
2014-08-15 23:39:49 -0700

    Complete port of DayViewController.

--- Eventual/Eventual/DayViewController.swift
+++ Eventual/Eventual/DayViewController.swift
@@ -7,10 +7,101 @@
 //
 
 import UIKit
+import EventKit
 
 @objc(ETDayViewController) class DayViewController: UICollectionViewController {
     
     var dayDate: NSDate?
+    
+    private lazy var titleFormatter: NSDateFormatter! = {
+        let titleFormatter = NSDateFormatter()
+        titleFormatter.dateFormat = "MMMM d"
+        return titleFormatter
+    }()
+    
+    // MARK: Data Source
+    
     var dayEvents: NSArray?
     
+    private let CellReuseIdentifier = "Event"
+    private var dataSource: NSArray? { return self.dayEvents }
+    
+    // MARK: Initializers
+    
+    init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        self.setUp()
+    }
+    init(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+        self.setUp()
+    }
+    
+    deinit {
+        self.tearDown()
+    }
+    
+    private func setUp() {}
+    private func tearDown() {}
+
+    // MARK: UIViewController
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setAccessibilityLabels()
+        if let dayDate = self.dayDate {
+            self.title = self.titleFormatter.stringFromDate(dayDate)
+        }
+    }
+    
+    private func setAccessibilityLabels() {
+        self.collectionView.isAccessibilityElement = true;
+        self.collectionView.accessibilityLabel = NSLocalizedString(ETLabel.DayEvents.toRaw(), comment: "");
+    }
 }
+
+extension DayViewController: UICollectionViewDataSource {
+    
+    override func collectionView(collectionView: UICollectionView!, numberOfItemsInSection section: Int) -> Int {
+        var number = 0
+        if let dataSource = self.dataSource {
+            number = dataSource.count
+        }
+        return number
+    }
+    
+    override func collectionView(collectionView: UICollectionView!, cellForItemAtIndexPath indexPath: NSIndexPath!) -> UICollectionViewCell! {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(CellReuseIdentifier, forIndexPath: indexPath) as EventViewCell
+        if let dataSource = self.dataSource {
+            if let event = dataSource[indexPath.item] as? EKEvent {
+                cell.eventText = event.title
+            }
+        }
+        return cell
+    }
+    
+}
+
+extension DayViewController: UICollectionViewDelegateFlowLayout {
+
+    // TODO: Move into nib.
+    func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: UICollectionViewLayout!, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 1.0
+    }
+    
+    func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: UICollectionViewLayout!, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 1.0
+    }
+    
+    func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: UICollectionViewLayout!, sizeForItemAtIndexPath indexPath: NSIndexPath!) -> CGSize {
+        return CGSize(width: self.collectionView.frame.size.width, height: 75.0)
+    }
+    
+    func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: UICollectionViewLayout!, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsZero
+    }
+    
+}
+
+
---
+++ Eventual/Eventual/EventViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  EventViewCell.swift
+//  Eventual
+//
+//  Created by Peng Wang on 8/16/14.
+//  Copyright (c) 2014 Hashtag Studio. All rights reserved.
+//
+
+import UIKit
+
+@objc(ETEventViewCell) class EventViewCell: UICollectionViewCell {
+    
+    var eventText: String? {
+    didSet {
+        if self.eventText == oldValue { return }
+        self.mainLabel.text = self.eventText
+    }
+    }
+    
+    @IBOutlet private weak var mainLabel: UILabel!
+    
+    override class func requiresConstraintBasedLayout() -> Bool {
+        return true
+    }
+    
+}
