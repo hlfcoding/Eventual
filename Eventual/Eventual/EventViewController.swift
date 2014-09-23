fbc1a689331fd9727f6c09bc039fe88c0fc32066
Peng Wang <peng@pengxwang.com>
2014-09-22 23:51:00 -0700

    Fixup dayLabel toggling in port of EventViewController.

--- Eventual/Eventual/EventViewController.swift
+++ Eventual/Eventual/EventViewController.swift
@@ -473,7 +473,7 @@ extension EventViewController {
     private func toggleDatePickerDrawerAppearance(visible: Bool) {
         if self.isDatePickerVisible == visible { return }
         self.datePickerDrawerHeightConstraint.constant = visible ? self.datePicker.frame.size.height : 1.0
-        self.dayLabel.hidden = true // TODO: Update layout?
+        self.dayLabel.hidden = visible // TODO: Update layout?
         self.updateLayoutForView(self.view, withDuration: DatePickerAppearanceTransitionDuration, options: .CurveEaseInOut) { finished in
             self.isDatePickerVisible = visible
             if visible {
