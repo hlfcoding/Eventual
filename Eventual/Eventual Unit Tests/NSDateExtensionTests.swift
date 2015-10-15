3cbb9f0dd51c1e8f6bb12111f6416aa119d26bbe
Peng Wang <peng@pengxwang.com>
2015-10-15 01:55:29 -0700

    Add NSDate EventManager extension tests, with some failing.

---
+++ Eventual/Eventual Unit Tests/NSDateExtensionTests.swift
@@ -0,0 +1,55 @@
+//
+//  NSDateExtensionTests.swift
+//  Eventual
+//
+//  Created by Peng Wang on 10/15/15.
+//  Copyright © 2015 Eventual App. All rights reserved.
+//
+
+import XCTest
+@testable import Eventual
+
+class NSDateExtensionTests: XCTestCase {
+
+    let calendar = NSCalendar.currentCalendar()
+    var midnight: NSDate!
+
+    override func setUp() {
+        super.setUp()
+        midnight = calendar.dateBySettingHour(0, minute: 0, second: 0, ofDate: NSDate(), options: [])!
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testDayDate() {
+        let afterMidnight = calendar.dateBySettingHour(1, minute: 1, second: 1, ofDate: NSDate(), options: [])!
+        XCTAssertEqual(afterMidnight.dayDate, midnight, "Truncates units below day.")
+    }
+    func testHourDate() {
+        let midnightAndChange = calendar.dateBySettingHour(0, minute: 1, second: 1, ofDate: NSDate(), options: [])!
+        XCTAssertEqual(midnightAndChange.hourDate, midnight, "Truncates units below hour.")
+    }
+    func testMonthDate() {
+        let middleOfMonth = calendar.dateWithEra(1, year: 2015, month: 1, day: 15, hour: 1, minute: 1, second: 1, nanosecond: 0)!
+        let startOfMonth = calendar.dateWithEra(1, year: 2015, month: 1, day: 1, hour: 0, minute: 0, second: 0, nanosecond: 0)!
+        XCTAssertEqual(middleOfMonth.monthDate, startOfMonth)
+    }
+
+    func testDayDateFromAddingDays() {
+        let endOfMonth = calendar.dateWithEra(1, year: 2015, month: 1, day: 31, hour: 23, minute: 59, second: 59, nanosecond: 0)!
+        let startOfNextMonth = calendar.dateWithEra(1, year: 2015, month: 2, day: 1, hour: 0, minute: 0, second: 0, nanosecond: 0)!
+        XCTAssertEqual(endOfMonth.dayDateFromAddingDays(1), startOfNextMonth, "Carries over to month unit when needed.")
+    }
+    func testHourDateFromAddingHours() {
+        let endOfToday = calendar.dateBySettingHour(23, minute: 59, second: 59, ofDate: NSDate(), options: [])!
+        let tomorrow = calendar.dateByAddingUnit(.Day, value: 1, toDate: midnight, options: [])
+        XCTAssertEqual(endOfToday.hourDateFromAddingHours(1), tomorrow, "Carries over to day unit when needed.")
+    }
+
+    func testDateWithTime() {
+        let endOfToday = calendar.dateBySettingHour(23, minute: 59, second: 59, ofDate: NSDate(), options: [])!
+        XCTAssertEqual(endOfToday.dateWithTime(midnight), midnight, "Wraps day unit when needed.")
+    }
+}
