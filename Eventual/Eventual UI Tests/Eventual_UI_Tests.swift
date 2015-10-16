155ba15fb2683727e2ee45f4f49b0db28880defd
Peng Wang <peng@pengxwang.com>
2015-10-16 02:26:36 -0700

    Minor UI test updates, but still not working.
    Something's wrong with our collection view.

--- Eventual/Eventual UI Tests/Eventual_UI_Tests.swift
+++ Eventual/Eventual UI Tests/Eventual_UI_Tests.swift
@@ -11,16 +11,13 @@ import XCTest
 
 class Eventual_UI_Tests: XCTestCase {
 
-    var app: XCUIApplication! // Otherwise the app instance isn't exactly the same.
-
     override func setUp() {
         super.setUp()
         // Put setup code here.
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        self.app = XCUIApplication()
-        self.app.launch()
+        XCUIApplication().launch()
     }
     
     override func tearDown() {
@@ -29,9 +26,14 @@ class Eventual_UI_Tests: XCTestCase {
     }
     
     func testNavigatingToExistingEvent() {
-        let app = self.app
-        // FIXME: This test fails because Xcode UI testing is half-baked, at least for collection view support.
-        app.collectionViews["Eventful Days By Month"].cells["Day Cell At Section 0 Item 0"].tap()
+        let app = XCUIApplication()
+
+        XCTAssert(app.collectionViews.element.exists)
+        let firstCell = app.cells.elementBoundByIndex(0)
+        let exists = NSPredicate(format: "exists == true")
+
+        expectationForPredicate(exists, evaluatedWithObject: firstCell, handler: nil)
+        waitForExpectationsWithTimeout(5, handler: nil)
     }
 
 }
