c0b5c61c6ee4ef4c3b0a31d87d9d263220e3f8b9
Peng Wang <peng@pengxwang.com>
2015-10-17 02:13:07 -0700

    Document addition from 096868c.

--- Eventual/Eventual UI Tests/UITestAdditions.swift
+++ Eventual/Eventual UI Tests/UITestAdditions.swift
@@ -11,6 +11,14 @@ import XCTest
 
 extension XCTestCase {
 
+    /**
+        To reduce boilerplate for UI tests, this wraps around `-expectationForPredicate:evaluatedWithObject`
+        and `-waitForExpectationsWithTimeout:handler:`.
+    
+        - parameter timeout: Defaults to `5` seconds.
+        - parameter then: Can be omitted so the expectation just fulfills without additional asserts.
+          The element is passed in, though in most cases it shouldn't be needed.
+    */
     func waitForElement(element: XCUIElement, timeout: NSTimeInterval?, then: ((XCUIElement) -> Void)?) {
         let predicate = NSPredicate(format: "exists == 1")
         expectationForPredicate(predicate, evaluatedWithObject: element) { () -> Bool in
