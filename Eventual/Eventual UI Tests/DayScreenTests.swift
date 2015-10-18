63e3755ec7f9c42db3fc86741d28d137b3adaf29
Peng Wang <peng@pengxwang.com>
2015-10-18 12:48:52 -0700

    Initial Day screen UI tests, with #navigateToDayScreen.

---
+++ Eventual/Eventual UI Tests/DayScreenTests.swift
@@ -0,0 +1,48 @@
+//
+//  DayScreenTests.swift
+//  Eventual
+//
+//  Created by Peng Wang on 10/18/15.
+//  Copyright © 2015 Eventual App. All rights reserved.
+//
+
+import XCTest
+
+class DayScreenTests: XCTestCase {
+
+    var app: XCUIApplication { return XCUIApplication() }
+    var collectionView: XCUIElement!
+    var navigationBar: XCUIElement!
+
+    override func setUp() {
+        super.setUp()
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        self.continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        self.app.launch()
+        self.collectionView = self.app.collectionViews[Label.DayEvents.rawValue]
+        self.navigationBar = self.app.navigationBars.element
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func navigateToDayScreen(then: () -> Void) {
+        let firstCell = self.app.cells[NSString(format: Label.FormatDayCell.rawValue, 0, 0) as String]
+        self.waitForElement(firstCell, timeout: nil) { (_) in
+            firstCell.tap()
+            XCTAssert(self.collectionView.exists)
+            then()
+        }
+    }
+
+    func testNavigatingToFirstEvent() {
+        self.navigateToDayScreen {
+            self.app.cells[NSString(format: Label.FormatEventCell.rawValue, 0) as String].tap()
+            XCTAssert(self.navigationBar.otherElements[Label.EventScreenTitle.rawValue].exists)
+        }
+    }
+
+}
