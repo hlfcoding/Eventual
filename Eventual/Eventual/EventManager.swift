8d91f2f4b0cf0a9ce7c8cc46b48f5499b1d97907
Peng Wang <peng@pengxwang.com>
2015-10-23 22:28:43 -0700

    Minor EventManager reorganization.

--- Eventual/Eventual/EventManager.swift
+++ Eventual/Eventual/EventManager.swift
@@ -34,61 +34,11 @@ class EventManager: NSObject {
         }
     }
 
-    // MARK: - Parsing
-
     var eventsByMonthsAndDays: DateIndexedEventCollection?
     func updateEventsByMonthsAndDays() {
         self.eventsByMonthsAndDays = self.arrangeToEventsByMonthsAndDays(self.events)
     }
-    func arrangeToEventsByMonthsAndDays(events: [NSObject]) -> DateIndexedEventCollection {
-        var months: [String: NSMutableArray] = [:]
-        let monthsDates: NSMutableArray = []
-        let monthsDays: NSMutableArray = []
-        for event in events {
-            // Months date array and days array.
-            guard let startDate = event.valueForKey("startDate") as? NSDate else { continue }
-            let monthDate = startDate.monthDate!
-            let monthIndex = monthsDates.indexOfObject(monthDate)
-            let needsNewMonth = monthIndex == NSNotFound
-            var days: [String: NSMutableArray] = needsNewMonth ? [:] : monthsDays[monthIndex] as! [String: NSMutableArray]
-            let daysDates: NSMutableArray = needsNewMonth ? [] : days[DatesKey]!
-            let daysEvents: NSMutableArray = needsNewMonth ? [] : days[EventsKey]!
-            if needsNewMonth {
-                monthsDates.addObject(monthDate)
-                days[DatesKey] = daysDates
-                days[EventsKey] = daysEvents
-                monthsDays.addObject(days)
-            }
-            // Days dates array and events array.
-            let dayDate = startDate.dayDate!
-            let dayIndex = daysDates.indexOfObject(dayDate)
-            let needsNewDay = dayIndex == NSNotFound
-            let dayEvents: NSMutableArray = needsNewDay ? [] : daysEvents[dayIndex] as! NSMutableArray
-            if needsNewDay {
-                daysDates.addObject(dayDate)
-                daysEvents.addObject(dayEvents)
-            }
-            dayEvents.addObject(event)
-        }
-        months[DatesKey] = monthsDates
-        months[DaysKey] = monthsDays
-        // TODO: Integrate with Settings bundle entry.
-        // print(months)
-        return months
-    }
-    
-    func eventsForDayDate(date: NSDate, months: DateIndexedEventCollection? = nil) -> NSArray {
-        // Find and select month, then day, then events from parsed events
-        guard let months = months ?? self.eventsByMonthsAndDays,
-                  monthDate = date.monthDate, dayDate = date.dayDate,
-                  monthIndex = months[DatesKey]?.indexOfObject(monthDate),
-                  days = months[DaysKey]?[monthIndex] as? DateIndexedEventCollection,
-              let dayIndex = days[DatesKey]?.indexOfObject(dayDate) where dayIndex != NSNotFound,
-              let dayEvents = days[EventsKey]?[dayIndex] as? NSArray
-              else { return [] }
-        return dayEvents
-    }
-    
+
     // MARK: - Initializers
 
     override init() {
@@ -126,6 +76,55 @@ class EventManager: NSObject {
 // MARK: - CRUD
 
 extension EventManager {
+
+    func arrangeToEventsByMonthsAndDays(events: [NSObject]) -> DateIndexedEventCollection {
+        var months: [String: NSMutableArray] = [:]
+        let monthsDates: NSMutableArray = []
+        let monthsDays: NSMutableArray = []
+        for event in events {
+            // Months date array and days array.
+            guard let startDate = event.valueForKey("startDate") as? NSDate else { continue }
+            let monthDate = startDate.monthDate!
+            let monthIndex = monthsDates.indexOfObject(monthDate)
+            let needsNewMonth = monthIndex == NSNotFound
+            var days: [String: NSMutableArray] = needsNewMonth ? [:] : monthsDays[monthIndex] as! [String: NSMutableArray]
+            let daysDates: NSMutableArray = needsNewMonth ? [] : days[DatesKey]!
+            let daysEvents: NSMutableArray = needsNewMonth ? [] : days[EventsKey]!
+            if needsNewMonth {
+                monthsDates.addObject(monthDate)
+                days[DatesKey] = daysDates
+                days[EventsKey] = daysEvents
+                monthsDays.addObject(days)
+            }
+            // Days dates array and events array.
+            let dayDate = startDate.dayDate!
+            let dayIndex = daysDates.indexOfObject(dayDate)
+            let needsNewDay = dayIndex == NSNotFound
+            let dayEvents: NSMutableArray = needsNewDay ? [] : daysEvents[dayIndex] as! NSMutableArray
+            if needsNewDay {
+                daysDates.addObject(dayDate)
+                daysEvents.addObject(dayEvents)
+            }
+            dayEvents.addObject(event)
+        }
+        months[DatesKey] = monthsDates
+        months[DaysKey] = monthsDays
+        // TODO: Integrate with Settings bundle entry.
+        // print(months)
+        return months
+    }
+
+    func eventsForDayDate(date: NSDate, months: DateIndexedEventCollection? = nil) -> NSArray {
+        // Find and select month, then day, then events from parsed events
+        guard let months = months ?? self.eventsByMonthsAndDays,
+                  monthDate = date.monthDate, dayDate = date.dayDate,
+                  monthIndex = months[DatesKey]?.indexOfObject(monthDate),
+                  days = months[DaysKey]?[monthIndex] as? DateIndexedEventCollection,
+              let dayIndex = days[DatesKey]?.indexOfObject(dayDate) where dayIndex != NSNotFound,
+              let dayEvents = days[EventsKey]?[dayIndex] as? NSArray
+              else { return [] }
+        return dayEvents
+    }
 
     func fetchEventsFromDate(startDate: NSDate = NSDate(),
                              untilDate endDate: NSDate,
