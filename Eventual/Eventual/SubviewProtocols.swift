30437abf2fb58054d1982118e62443a5816714c6
Peng Wang <peng@pengxwang.com>
2016-02-27 00:34:06 -0800

    Extract base protocols files from defines.

---
+++ Eventual/Eventual/SubviewProtocols.swift
@@ -0,0 +1,15 @@
+//
+//  SubviewProtocols.swift
+//  Eventual
+//
+//  Created by Peng Wang on 2/26/16.
+//  Copyright (c) 2016 Eventual App. All rights reserved.
+//
+
+import UIKit
+
+protocol NavigationTitleViewProtocol: NSObjectProtocol {
+
+    var textColor: UIColor! { get set }
+    
+}
---
+++ Eventual/Eventual/TransitionProtocols.swift
@@ -0,0 +1,62 @@
+//
+//  TransitionProtocols.swift
+//  Eventual
+//
+//  Created by Peng Wang on 2/26/16.
+//  Copyright © 2016 Eventual App. All rights reserved.
+//
+
+import UIKit
+
+@objc(ETTransitionAnimationDelegate) protocol TransitionAnimationDelegate: NSObjectProtocol {
+
+    func animatedTransition(transition: AnimatedTransition,
+         snapshotReferenceViewWhenReversed reversed: Bool) -> UIView
+
+    optional func animatedTransition(transition: AnimatedTransition,
+                  willCreateSnapshotViewFromReferenceView reference: UIView)
+
+    optional func animatedTransition(transition: AnimatedTransition,
+                  didCreateSnapshotView snapshot: UIView, fromReferenceView reference: UIView)
+
+    optional func animatedTransition(transition: AnimatedTransition,
+                  willTransitionWithSnapshotReferenceView reference: UIView, reversed: Bool)
+
+    optional func animatedTransition(transition: AnimatedTransition,
+                  didTransitionWithSnapshotReferenceView reference: UIView, reversed: Bool)
+
+    optional func animatedTransition(transition: AnimatedTransition,
+                  subviewsToAnimateSeparatelyForReferenceView reference: UIView) -> [UIView]
+
+    optional func animatedTransition(transition: AnimatedTransition,
+                  subviewInDestinationViewController viewController: UIViewController,
+                  forSubview subview: UIView) -> UIView?
+}
+
+protocol TransitionInteractionDelegate: NSObjectProtocol {
+
+    func interactiveTransition(transition: InteractiveTransition,
+         locationContextViewForGestureRecognizer recognizer: UIGestureRecognizer) -> UIView
+
+    func interactiveTransition(transition: InteractiveTransition,
+         snapshotReferenceViewAtLocation location: CGPoint, ofContextView contextView: UIView) -> UIView?
+
+    func beginInteractivePresentationTransition(transition: InteractiveTransition,
+         withSnapshotReferenceView referenceView: UIView?)
+
+    func beginInteractiveDismissalTransition(transition: InteractiveTransition,
+         withSnapshotReferenceView referenceView: UIView?)
+
+    func interactiveTransition(transition: InteractiveTransition,
+         destinationScaleForSnapshotReferenceView referenceView: UIView?,
+         contextView: UIView, reversed: Bool) -> CGFloat
+
+}
+
+@objc(ETAnimatedTransition) protocol AnimatedTransition: UIViewControllerAnimatedTransitioning {}
+
+@objc(ETInteractiveTransition) protocol InteractiveTransition: UIViewControllerInteractiveTransitioning {
+
+    var isEnabled: Bool { get set }
+
+}
