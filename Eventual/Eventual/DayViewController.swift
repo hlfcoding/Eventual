hlfcoding/Eventual

commit 87208a1ba06ed961bf90cdd63d6b3c9e58a70f6d

Fix transition regression from unwindToDay changes.
Was always using fallback transition.

Eventual/Eventual/DayViewController.swift
@@ -145,16 +145,16 @@ extension DayViewController: TransitionAnimationDelegate, TransitionInteractionD
         if let navigationController = self.presentedViewController as? NavigationViewController,
                indexPath = self.currentIndexPath
         {
-            // Just do the default transition if the snapshotReferenceView is illegitimate.
             self.eventManager.updateEventsByMonthsAndDays() // FIXME
             self.updateData()
+            self.collectionView!.reloadData()
             if self.events?.count > indexPath.item,
                let event = self.events?[indexPath.item] where event.startDate != self.dayDate // Is date modified?
             {
-                self.collectionView!.reloadData()
+                // Just do the default transition if the snapshotReferenceView is illegitimate.
+                navigationController.transitioningDelegate = nil
+                navigationController.modalPresentationStyle = .FullScreen
             }
-            navigationController.transitioningDelegate = nil
-            navigationController.modalPresentationStyle = .FullScreen
         }
     }
 

commit 5a562a572ac519dcdef61e6f975c9096f114ff73

Fix crash on Day screen after moving last event.
87208a1 introduced the regression. This fixes that without regressions.

Eventual/Eventual/DayViewController.swift
@@ -137,14 +137,19 @@ extension DayViewController: CollectionViewBackgroundTapTraitDelegate,
 
     @IBAction private func unwindToDay(sender: UIStoryboardSegue) {
         if let navigationController = self.presentedViewController as? NavigationViewController,
-               indexPath = self.currentIndexPath
+               indexPath = self.currentIndexPath,
+               events = self.events
         {
             self.eventManager.updateEventsByMonthsAndDays() // FIXME
             self.updateData()
             self.collectionView!.reloadData()
-            if self.events?.count > indexPath.item,
-               let event = self.events?[indexPath.item] where event.startDate.dayDate != self.dayDate // Is date modified?
-            {
+
+            // Empty if moved to different day.
+            var isCurrentEventInDay = !self.events.isEmpty
+            if events.count > indexPath.item && events[indexPath.item].startDate.dayDate != self.dayDate {
+                isCurrentEventInDay = false
+            }
+            if !isCurrentEventInDay {
                 // Just do the default transition if the snapshotReferenceView is illegitimate.
                 navigationController.transitioningDelegate = nil
                 navigationController.modalPresentationStyle = .FullScreen