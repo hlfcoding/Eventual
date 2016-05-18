8c722fedf7a0ec7f194da7abcb9faac02ffebdfd
Peng Wang <peng@pengxwang.com>
2016-05-17 22:34:27 -0700

    Prevent crash on out-of-bounds error on first load.

--- Eventual/Eventual/MonthsViewController.swift
+++ Eventual/Eventual/MonthsViewController.swift
@@ -412,7 +412,10 @@ extension MonthsViewController: NavigationTitleScrollViewDataSource, NavigationT
     func navigationTitleScrollView(scrollView: NavigationTitleScrollView, itemAtIndex index: Int) -> UIView? {
         var titleText: NSString?
         var label: UILabel?
-        if let month = self.months?[index] as? NSDate {
+        if
+            let months = self.months where months.count > index,
+            let month = months[index] as? NSDate
+        {
             titleText = MonthHeaderView.formattedTextForText(NSDateFormatter.monthFormatter.stringFromDate(month))
         }
         if let info = NSBundle.mainBundle().infoDictionary {
