707a7e629630c0a2e1c87644406c40be16dd4b3c
Peng Wang <peng@pengxwang.com>
2016-07-12 02:41:14 -0700

    Simplify and fix UI test helper (further break tests).
    Synchronous and correct. The callback was invoked in the wrong place.

--- Eventual/Eventual UI Tests/UITestAdditions.swift
+++ Eventual/Eventual UI Tests/UITestAdditions.swift
@@ -13,19 +13,10 @@ extension XCTestCase {
     /**
      To reduce boilerplate for UI tests, this wraps around `-expectationForPredicate:evaluatedWithObject`
      and `-waitForExpectationsWithTimeout:handler:`.
-     - parameter timeout: Defaults to `5` seconds.
-     - parameter then: Can be omitted so the expectation just fulfills without additional asserts.
-       The element is passed in, though in most cases it shouldn't be needed.
     */
-    func waitForElement(element: XCUIElement, timeout: NSTimeInterval? = nil,
-                        then: ((XCUIElement) -> Void)? = nil)
-    {
-        let predicate = NSPredicate(format: "exists == 1")
-        expectationForPredicate(predicate, evaluatedWithObject: element) { () -> Bool in
-            then?(element)
-            return true
-        }
-        waitForExpectationsWithTimeout(timeout ?? 5, handler: nil)
+    func waitForElement(element: XCUIElement) {
+        expectationForPredicate(NSPredicate(format: "exists == true"), evaluatedWithObject: element, handler: nil)
+        waitForExpectationsWithTimeout(5, handler: nil)
     }
 
 }
