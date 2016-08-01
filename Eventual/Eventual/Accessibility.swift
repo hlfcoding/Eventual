c609385b2dc278a0f481fa934365df0d7654efe0
Peng Wang <peng@pengxwang.com>
2016-07-31 22:58:16 -0700

    Factor out Accessibility aspect.

---
+++ Eventual/Eventual/Accessibility.swift
@@ -0,0 +1,26 @@
+//
+//  Accessibility.swift
+//  Eventual
+//
+//  Copyright (c) 2014-present Eventual App. All rights reserved.
+//
+
+enum Label: String {
+    case AddDayEvent = "Add Event To Day"
+    case AddEvent = "Add Event"
+    case DayEvents = "Day Events"
+    case EventDate = "Event Date"
+    case EventDescription = "Event Description"
+    case EventTime = "Event Time"
+    case EventScreenTitle = "Event Screen Title"
+    case FormatDayCell = "Day Cell At Section %d Item %d"
+    case FormatDayOption = "Day Option Named %@"
+    case FormatEventCell = "Event Cell At Item %d"
+    case MonthDays = "Eventful Days By Month"
+    case MonthsScreenTitle = "Months Screen Title"
+    case NavigationBack = "Back"
+    case PickDate = "Pick Date"
+    case PickTime = "Pick Time"
+    case SaveEvent = "Save Event"
+    case TappableBackground = "Tappable Background"
+}
