371f78b9f3150a205f5bb5b42d6a2d253c7eaa22
Peng Wang <peng@pengxwang.com>
2016-08-11 10:17:51 -0700

    Minor reorganization, encapsulation.

--- Eventual/Eventual/CollectionViewFlowLayoutFluidity.swift
+++ Eventual/Eventual/CollectionViewFlowLayoutFluidity.swift
@@ -7,26 +7,37 @@
 
 import UIKit
 
+/**
+ Equal-width item sizing and stretching to fill the available width, with the remainder going to
+ `rowSpaceRemainder`. Values are always computed. A `sizeMultiplier` can be configured. Inspired by
+ [CSS fluid layouts](https://smashingmagazine.com/2009/06/fixed-vs-fluid-vs-elastic-layout-whats-the-right-one-for-you/#fluid-website-layouts).
+ */
 struct CollectionViewFlowLayoutFluidity {
 
-    weak var layout: UICollectionViewFlowLayout!
-
-    var itemSize: CGSize {
-        return CGSize(width: dimension, height: isSquare ? dimension : resizedDesiredItemHeight)
-    }
-    var numberOfColumns: Int {
-        return Int(availableWidth / resizedDesiredItemWidth)
-    }
-    var rowSpaceRemainder: Int {
-        return Int(availableWidth - (dimension * columns))
-    }
-    var sizeMultiplier: CGFloat = 1
-
-    private var desiredItemSize: CGSize!
+    // MARK: Level 1 Calculations
 
     private var availableWidth: CGFloat {
         return layout.collectionView!.frame.width - (layout.sectionInset.left + layout.sectionInset.right)
     }
+    private var desiredItemHeight: CGFloat {
+        return desiredItemSize.height * sizeMultiplier
+    }
+    private var desiredItemWidth: CGFloat {
+        guard availableWidth > desiredItemSize.width else {
+            return desiredItemSize.width
+        }
+        return desiredItemSize.width * sizeMultiplier
+    }
+    private var isSquare: Bool {
+        return desiredItemSize.width == desiredItemSize.height
+    }
+
+    // MARK: Level 2 Calculations
+
+    var numberOfColumns: Int {
+        return Int(availableWidth / desiredItemWidth)
+    }
+
     private var columns: CGFloat {
         return CGFloat(numberOfColumns)
     }
@@ -35,18 +46,21 @@ struct CollectionViewFlowLayoutFluidity {
         let availableCellWidth = availableWidth - (gutters * layout.minimumInteritemSpacing)
         return floor(availableCellWidth / columns)
     }
-    private var isSquare: Bool {
-        return desiredItemSize.width == desiredItemSize.height
+
+    // MARK: Level 3 Calculations
+
+    var itemSize: CGSize {
+        return CGSize(width: dimension, height: isSquare ? dimension : desiredItemHeight)
     }
-    private var resizedDesiredItemHeight: CGFloat {
-        return desiredItemSize.height * sizeMultiplier
+    var rowSpaceRemainder: Int {
+        return Int(availableWidth - (dimension * columns))
     }
-    private var resizedDesiredItemWidth: CGFloat {
-        guard availableWidth > desiredItemSize.width else {
-            return desiredItemSize.width
-        }
-        return desiredItemSize.width * sizeMultiplier
-    }
+
+    // MARK: Configuration
+
+    private(set) weak var layout: UICollectionViewFlowLayout!
+    private(set) var desiredItemSize: CGSize!
+    var sizeMultiplier: CGFloat = 1
 
     init(layout: UICollectionViewFlowLayout, desiredItemSize: CGSize) {
         self.layout = layout
