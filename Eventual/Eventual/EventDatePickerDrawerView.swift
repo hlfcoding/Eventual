3128dfdb7d65ef0267fe45b1738f7299c6f69d92
Peng Wang <peng@pengxwang.com>
2016-08-15 23:49:41 -0700

    Blank EventDatePickerDrawerView.

---
+++ Eventual/Eventual/EventDatePickerDrawerView.swift
@@ -0,0 +1,12 @@
+//
+//  EventDatePickerDrawerView.swift
+//  Eventual
+//
+//  Copyright (c) 2014-present Eventual App. All rights reserved.
+//
+
+import UIKit
+
+class EventDatePickerDrawerView: UIView {
+
+}
