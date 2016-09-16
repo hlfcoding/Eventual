5cc6f7b149b55a944cc4d4e75a19dbdac6878f53
Peng Wang <peng@pengxwang.com>
2016-09-16 00:30:27 -0700

    Migrate Additions.swift.

--- Eventual/Eventual/Additions.swift
+++ Eventual/Eventual/Additions.swift
@@ -16,10 +16,10 @@ import EventKit
  - parameter comment: Optional, but useful for the translator.
  - parameter argument: Singular and not variadic supported because Swift doesn't have splats.
  */
-func t<Key: StringLiteralConvertible>(key: Key, _ comment: String? = "", _ argument: CVarArgType? = nil) -> String {
-    let localized = NSLocalizedString(String(key), comment: comment!)
+func t<Key: ExpressibleByStringLiteral>(_ key: Key, _ comment: String? = "", _ argument: CVarArg? = nil) -> String {
+    let localized = NSLocalizedString(String(describing: key), comment: comment!)
     if let argument = argument {
-        return NSString.localizedStringWithFormat(localized, argument) as String
+        return NSString.localizedStringWithFormat(localized as NSString, argument) as String
     }
     return localized
 }
@@ -28,18 +28,17 @@ func t<Key: StringLiteralConvertible>(key: Key, _ comment: String? = "", _ argum
  `a` is short for `accessibility` and shortens accessibility label code.
  - parameter argument: Singular and not variadic supported because Swift doesn't have splats.
  */
-func a(key: Label, _ argument: CVarArgType? = nil) -> String {
+func a(_ key: Label, _ argument: CVarArg? = nil) -> String {
     return t(key.rawValue, "accessibility", argument)
 }
 
-func dispatchAfter(duration: NSTimeInterval, block: dispatch_block_t!) {
-    let time = Int64(duration * Double(NSEC_PER_SEC))
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, time), dispatch_get_main_queue(), block)
+func dispatchAfter(_ duration: TimeInterval, block: @escaping () -> Void) {
+    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + duration, execute: block)
 }
 
 class NotificationPayload {
 
-    private static let key = "payload"
+    fileprivate static let key = "payload"
 
     var userInfo: UserInfo { return [ NotificationPayload.key: self ] }
 
@@ -72,34 +71,36 @@ extension CGRect {
 
 }
 
-extension NSCalendarUnit {
+extension Calendar.Component {
 
-    static let dayUnitFlags: NSCalendarUnit = [.Day, .Month, .Year]
-    static let hourUnitFlags: NSCalendarUnit = [.Hour, .Day, .Month, .Year]
-    static let monthUnitFlags: NSCalendarUnit = [.Month, .Year]
+    static let dayComponents: Set<Calendar.Component> = [.day, .month, .year]
+    static let hourComponents: Set<Calendar.Component> = [.hour, .day, .month, .year]
+    static let monthComponents: Set<Calendar.Component> = [.month, .year]
 
 }
 
-extension NSDate {
+extension Date {
 
     /**
      New date based on this date where everything smaller than the day component is 0.
      `numberOfDays` can be negative to get an earlier date.
      */
-    func dayDateFromAddingDays(numberOfDays: Int) -> NSDate {
-        let calendar = NSCalendar.currentCalendar(), components = NSDateComponents()
+    func dayDateFromAddingDays(_ numberOfDays: Int) -> Date {
+        let calendar = Calendar.current
+        var components = DateComponents()
         components.day = numberOfDays
-        return calendar.dateByAddingComponents(components, toDate: self, options: [])!.dayDate
+        return calendar.date(byAdding: components, to: self)!.dayDate
     }
 
     /**
      New date based on this date where everything smaller than the hour component is 0.
      `numberOfHours` can be negative to get an earlier date.
      */
-    func hourDateFromAddingHours(numberOfHours: Int) -> NSDate {
-        let calendar = NSCalendar.currentCalendar(), components = NSDateComponents()
+    func hourDateFromAddingHours(_ numberOfHours: Int) -> Date {
+        let calendar = Calendar.current
+        var components = DateComponents()
         components.hour = numberOfHours
-        return calendar.dateByAddingComponents(components, toDate: self, options: [])!.hourDate
+        return calendar.date(byAdding: components, to: self)!.hourDate
     }
 
     var hasCustomTime: Bool {
@@ -109,62 +110,62 @@ extension NSDate {
     /**
      New date based on this date, combined with `timeDate`'s time components.
      */
-    func dateWithTime(timeDate: NSDate) -> NSDate {
-        let calendar = NSCalendar.currentCalendar()
-        let timeComponents = calendar.components([.Hour, .Minute, .Second], fromDate: timeDate)
-        return calendar.dateBySettingHour(timeComponents.hour, minute: timeComponents.minute, second: timeComponents.second, ofDate: self, options: [.WrapComponents])!
+    func dateWithTime(_ timeDate: Date) -> Date {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents(Set<Calendar.Component>([.hour, .minute, .second]), from: timeDate)
+        return calendar.date(bySettingHour: components.hour!, minute: components.minute!, second: components.second!, of: self)!
     }
 
     /**
      This conversion from a valid `NSDate` to normalized `NSDateComponents` and back is obviously
      safe, hence the forced unwrapping.
      */
-    private func flooredDateWithComponents(unitFlags: NSCalendarUnit) -> NSDate {
-        let calendar = NSCalendar.currentCalendar()
-        return calendar.dateFromComponents(calendar.components(unitFlags, fromDate: self))!
+    private func flooredDate(with components: Set<Calendar.Component>) -> Date {
+        let calendar = Calendar.current
+        return calendar.date(from: calendar.dateComponents(components, from: self))!
     }
 
     /** Clone except everything smaller than the day component is 0. */
-    var dayDate: NSDate { return flooredDateWithComponents(NSCalendarUnit.dayUnitFlags) }
+    var dayDate: Date { return flooredDate(with: Calendar.Component.dayComponents) }
 
     /** Clone except everything smaller than the hour component is 0. */
-    var hourDate: NSDate { return flooredDateWithComponents(NSCalendarUnit.hourUnitFlags) }
+    var hourDate: Date { return flooredDate(with: Calendar.Component.hourComponents) }
 
     /** Clone except everything smaller than the month component is 0. */
-    var monthDate: NSDate { return flooredDateWithComponents(NSCalendarUnit.monthUnitFlags) }
+    var monthDate: Date { return flooredDate(with: Calendar.Component.monthComponents) }
 
 }
 
-extension NSDateFormatter {
+extension DateFormatter {
 
     // NOTE: This memory can never be freed as long as app is active. But since every screen
     // requires at least one form of formatting, it's reasonable to always keep this in memory.
-    private static var sharedDateFormatter = NSDateFormatter()
+    private static var sharedDateFormatter = DateFormatter()
 
-    private static func sharedDateFormatterWithFormat(dateFormat: String) -> NSDateFormatter {
-        let formatter = NSDateFormatter.sharedDateFormatter
-        formatter.dateFormat = dateFormat
+    private static func sharedDateFormatter(format: String) -> DateFormatter {
+        let formatter = DateFormatter.sharedDateFormatter
+        formatter.dateFormat = format
         return formatter
     }
 
-    static var dayFormatter: NSDateFormatter { return NSDateFormatter.sharedDateFormatterWithFormat("d") }
+    static var dayFormatter: DateFormatter { return DateFormatter.sharedDateFormatter(format: "d") }
 
-    static var dateFormatter: NSDateFormatter { return NSDateFormatter.sharedDateFormatterWithFormat("MMMM d, y · EEEE") }
+    static var dateFormatter: DateFormatter { return DateFormatter.sharedDateFormatter(format: "MMMM d, y · EEEE") }
 
-    static var monthFormatter: NSDateFormatter { return NSDateFormatter.sharedDateFormatterWithFormat("MMMM") }
+    static var monthFormatter: DateFormatter { return DateFormatter.sharedDateFormatter(format: "MMMM") }
 
-    static var monthDayFormatter: NSDateFormatter { return NSDateFormatter.sharedDateFormatterWithFormat("MMMM d") }
+    static var monthDayFormatter: DateFormatter { return DateFormatter.sharedDateFormatter(format: "MMMM d") }
 
-    static var timeFormatter: NSDateFormatter { return NSDateFormatter.sharedDateFormatterWithFormat("h:mm a") }
+    static var timeFormatter: DateFormatter { return DateFormatter.sharedDateFormatter(format: "h:mm a") }
 
-    static var accessibleDateFormatter: NSDateFormatter { return NSDateFormatter.sharedDateFormatterWithFormat("EEEE, MMMM d, y") }
+    static var accessibleDateFormatter: DateFormatter { return DateFormatter.sharedDateFormatter(format: "EEEE, MMMM d, y") }
 
 }
 
 extension Dictionary {
 
     func notificationUserInfoPayload() -> AnyObject? {
-        return ((self as? AnyObject) as? UserInfo)?[NotificationPayload.key]
+        return ((self as AnyObject) as? UserInfo)?[NotificationPayload.key]
     }
     
 }
@@ -174,11 +175,11 @@ extension String {
     /**
      Addition. A simple JSON-like debug formatter.
      */
-    static func debugDescriptionForGroupWithLabel(label: String, attributes: [String: AnyObject?],
-                                                  indentLevel: Int = 0) -> String {
-        let tab = Character("\t")
-        let outerIndent = String(count: indentLevel, repeatedValue: tab)
-        let innerIndent = String(count: indentLevel + 1, repeatedValue: tab)
+    static func debugDescriptionForGroup(label: String, attributes: [String: String?],
+                                         indentLevel: Int = 0) -> String {
+        let tab = "\t"
+        let outerIndent = String(repeating: tab, count: indentLevel)
+        let innerIndent = String(repeating: tab, count: indentLevel + 1)
         return (
             "\(outerIndent)\(label): {\n" +
             attributes.reduce("") { $0 + "\(innerIndent)\($1.0): \($1.1)\n" } +
@@ -212,20 +213,20 @@ extension UIView {
      Addition. Animates layout constraint changes immediately, with animation and spring options
      that have an app-specific base.
      */
-    func animateLayoutChangesWithDuration(duration: NSTimeInterval, usingSpring: Bool = true,
-                                          options: UIViewAnimationOptions, completion: ((Bool) -> Void)?) {
+    func animateLayoutChanges(duration: TimeInterval, usingSpring: Bool = true,
+                              options: UIViewAnimationOptions, completion: ((Bool) -> Void)?) {
         let animations = { self.layoutIfNeeded() }
         setNeedsUpdateConstraints()
         if usingSpring {
             let (damping, initialVelocity) = Appearance.drawerSpringAnimation
-            UIView.animateWithDuration(
-                duration, delay: 0,
+            UIView.animate(
+                withDuration: duration, delay: 0,
                 usingSpringWithDamping: damping, initialSpringVelocity: initialVelocity,
                 options: options, animations: animations, completion: completion
             )
         } else {
-            UIView.animateWithDuration(
-                duration, delay: 0,
+            UIView.animate(
+                withDuration: duration, delay: 0,
                 options: options, animations: animations, completion: completion
             )
         }
@@ -235,8 +236,8 @@ extension UIView {
     /**
      Addition. Pythagoras distance based durations. `time = distance / rate`
      */
-    static func durationForAnimatingBetweenPoints(points: (CGPoint, CGPoint),
-                                                  withVelocity pointsPerSecond: Double) -> NSTimeInterval {
+    static func durationForAnimatingBetweenPoints(_ points: (CGPoint, CGPoint),
+                                                  withVelocity pointsPerSecond: Double) -> TimeInterval {
         let (a, b) = points
         let distance = Double(sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2)))
         return distance / pointsPerSecond
@@ -252,13 +253,13 @@ extension UIViewController {
      */
     func customizeNavigationItem() {
         if let title = navigationItem.title {
-            navigationItem.title = title.uppercaseString
+            navigationItem.title = title.uppercased()
         }
 
         func customizeBackLeftItem() {
-            guard let buttonItem = navigationItem.leftBarButtonItem where buttonItem.title == Label.NavigationBack.rawValue else { return }
+            guard let buttonItem = navigationItem.leftBarButtonItem, buttonItem.title == Label.NavigationBack.rawValue else { return }
             guard let iconFont = UIFont(name: Appearance.fontName, size: Appearance.iconBarButtonItemFontSize) else { return }
-            buttonItem.setTitleTextAttributes([ NSFontAttributeName: iconFont ], forState: .Normal)
+            buttonItem.setTitleTextAttributes([ NSFontAttributeName: iconFont ], for: .normal)
             buttonItem.accessibilityLabel = a(.NavigationBack)
             buttonItem.title = Icon.LeftArrow.rawValue
         }
