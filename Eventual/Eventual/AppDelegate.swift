b27b7b985b962af44d7da276d5b981029f71e3fa
Peng Wang <peng@pengxwang.com>
2016-09-25 11:17:43 -0700

    Migrate AppDelegate.swift.

--- Eventual/Eventual/AppDelegate.swift
+++ Eventual/Eventual/AppDelegate.swift
@@ -12,47 +12,50 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    lazy var eventManager = EventManager()
+    lazy var eventManager: EventManager = EventManager()
     lazy var mainCoordinator: NavigationCoordinator = {
         return NavigationCoordinator(eventManager: self.eventManager)
     }()
 
     static var sharedDelegate: AppDelegate {
         guard
-            let delegate = UIApplication.sharedApplication().delegate as? AppDelegate
+            let delegate = UIApplication.shared.delegate as? AppDelegate
             else { preconditionFailure("No app delegate!") }
 
         return delegate
     }
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         Appearance.apply()
-        guard let navigationController = window?.rootViewController as? UINavigationController else { preconditionFailure() }
+        guard let navigationController = window?.rootViewController as? UINavigationController
+            else { preconditionFailure() }
         navigationController.delegate = mainCoordinator
-        guard let viewController = navigationController.topViewController as? CoordinatedViewController else { preconditionFailure() }
+        guard let viewController = navigationController.topViewController as? CoordinatedViewController
+            else { preconditionFailure() }
         viewController.coordinator = mainCoordinator
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
+    func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
-    func applicationWillEnterForeground(application: UIApplication) {
+    func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
+    func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(application: UIApplication) {
+    func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
