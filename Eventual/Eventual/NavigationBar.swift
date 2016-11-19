f3d879f45964ba6bb886e51d3573023febc8b35a
Peng Wang <peng@pengxwang.com>
2016-11-19 00:08:29 -0800

    Rework UINavigationBar customization to keep blur.
    Only a little more fragile.

--- Eventual/Eventual/NavigationBar.swift
+++ Eventual/Eventual/NavigationBar.swift
@@ -10,32 +10,24 @@ import UIKit
 
 class NavigationBar: UINavigationBar {
 
+    var customShadow: UIView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customShadow = UIView(frame: .zero)
+        customShadow.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(customShadow)
+        NSLayoutConstraint.activate([
+            customShadow.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 1),
+            customShadow.leadingAnchor.constraint(equalTo: leadingAnchor),
+            customShadow.trailingAnchor.constraint(equalTo: trailingAnchor),
+            customShadow.heightAnchor.constraint(equalToConstant: 1),
+        ])
+    }
+
     override func tintColorDidChange() {
         super.tintColorDidChange()
-        applyCustomBorder(color: tintColor)
-    }
-
-    private func applyCustomBorder(color: UIColor, backgroundColor: UIColor = UIColor(white: 1, alpha: 0.95)) {
-        // Temporary appearance changes.
-        for view in subviews {
-            view.backgroundColor = UIColor.clear
-        }
-        // Custom bar border color, at the cost of translucency.
-        let bgHeight = frame.height + UIApplication.shared.statusBarFrame.height
-        let bgImage = createColorImage(color: backgroundColor,
-                                       size: CGSize(width: frame.width, height: bgHeight))
-        setBackgroundImage(bgImage, for: .default)
-        shadowImage = createColorImage(color: color, size: CGSize(width: frame.width, height: 1))
-    }
-
-    private func createColorImage(color: UIColor, size: CGSize) -> UIImage {
-        UIGraphicsBeginImageContext(size)
-        let path = UIBezierPath(rect: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-        color.setFill()
-        path.fill()
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return image!
+        customShadow.backgroundColor = tintColor
     }
 
 }
