9201cf6e604b059de3ad0f7761b47981c6f246b7
Peng Wang <peng@pengxwang.com>
2017-05-02 00:33:59 -0700

    Initial ActiveLabel implementation, combining...
    - stackoverflow.com/q/36043006
    - stackoverflow.com/q/19332283

--- Eventual/Eventual/ActiveLabel.swift
+++ Eventual/Eventual/ActiveLabel.swift
@@ -13,6 +13,40 @@ class ActiveLabel: UILabel {
 
     weak var actionSender: NSObjectProtocol?
 
+    private var actionBoundingRects = [String : CGRect]()
+
+    override var attributedText: NSAttributedString? {
+        didSet {
+            guard isUserInteractionEnabled else { return }
+            actionBoundingRects.removeAll()
+            guard let attributedText = attributedText, attributedText.length > 0 else { return }
+
+            let layoutManager = NSLayoutManager()
+            let textStorage = NSTextStorage(attributedString: attributedText)
+            let textContainer = NSTextContainer(size: bounds.size)
+            textContainer.lineFragmentPadding = 0
+            textContainer.lineBreakMode = lineBreakMode
+            layoutManager.addTextContainer(textContainer)
+            textStorage.addLayoutManager(layoutManager)
+
+            var glyphRange = NSRange()
+            var index = 0
+            repeat {
+                if let attribute = attributedText.attribute(
+                    ActiveLabel.actionAttributeName, at: index, effectiveRange: &glyphRange)
+                    as? String,
+                    glyphRange.length > 0 {
+                    actionBoundingRects[attribute] = layoutManager.boundingRect(
+                        forGlyphRange: glyphRange, in: layoutManager.textContainers.first!)
+                    index = glyphRange.location + glyphRange.length
+                } else {
+                    index += 1
+                }
+            } while index < attributedText.length
+            print(actionBoundingRects)
+        }
+    }
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setUp()
@@ -30,7 +64,13 @@ class ActiveLabel: UILabel {
     }
 
     @objc private func detectFragmentTap(_ sender: UITapGestureRecognizer) {
-
+        let location = sender.location(in: self)
+        for (selector, boundingRect) in actionBoundingRects {
+            if boundingRect.contains(location) {
+                UIApplication.shared.sendAction(Action(rawValue: selector)!, from: actionSender)
+                break
+            }
+        }
     }
 
 }
